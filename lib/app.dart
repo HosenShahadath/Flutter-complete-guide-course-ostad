84e79d89fb9085bc173d26ed3227c52ebf4e17bb
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-16 07:31:15 +0600

    class 1

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice_ostad/module%207/recoded%20video/bottom%20nav/bottom%20nav.dart';
-import 'package:flutter_practice_ostad/module%207/recoded%20video/navigation%20drawer/navigation%20drawer.dart';
+import 'package:flutter_practice_ostad/module%207/live%20class/class%201.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavDrawer(),
+      home: Class1(),
     );
   }
 }
---
+++ lib/module 7/live class/class 1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Class1 extends StatelessWidget {
+  const Class1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
